69b787f795b06b46a95bf66f698cd9cae353b47c
Matheus Gusmão <mgusmao.dev@gmail.com>
2026-01-09 01:23:03 -0300

    feat(model/errors): create standardized codes and messages for errors in data validation

---
+++ src/supermarket/model/errors.clj
@@ -0,0 +1,15 @@
+(ns supermarket.model.errors)
+
+;; errors code:
+;; E001 -> Without field
+;; E002 -> Invalid type
+;; E003 -> Invalid value format
+
+(defn ->without-field-error [field]
+  (format "E001: The '%s' field is required." (name field)))
+
+(defn ->invalid-type-error [field type]
+  (format "E002: The '%s' field must be of type '%s'." (name field) (name type)))
+
+(defn ->invalid-value-error [field custom-message]
+  (format "E003: The '%s' field must be %s." field custom-message))
