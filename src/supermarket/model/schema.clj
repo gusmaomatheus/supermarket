24725119ea0f09c60c6f54571a09229dbcffe67c
Matheus Gusmão <mgusmao.dev@gmail.com>
2026-01-09 00:43:48 -0300

    feat(model): create customer entity schema

---
+++ src/supermarket/model/schema.clj
@@ -0,0 +1,8 @@
+(ns supermarket.model.schema)
+
+(def CustomerSchema
+  [:map
+   [:document :string]
+   [:status [:enum "ACTIVATED" "DISABLED"]]
+   [:type [:enum "COMMON" "SPECIAL"]]
+   [:registrationDate :string]])
