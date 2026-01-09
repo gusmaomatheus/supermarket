850f104b0d56c51f50451c00579716200d357309
Matheus Gusmão <mgusmao.dev@gmail.com>
2026-01-09 01:04:54 -0300

    test(schema): validating the customer entity schema

---
+++ test/supermarket/model/schema_test.clj
@@ -0,0 +1,32 @@
+(ns supermarket.model.schema-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [supermarket.model.schema :as schema]
+            [supermarket.model.validator :refer [schema-validate]]))
+
+;; mocks
+
+(def valid-customer
+  {:document "49184711516"
+   :status "ACTIVATED"
+   :type "COMMON"
+   :registration-date "2025-01-09"})
+
+(def invalid-customer
+  {:document 49184711516
+   :status "ON"
+   :type "RARE"
+   :registration-date 741893198})
+
+;; tests
+
+;; TODO:
+;; 1. Adicionar mensagem de erros personalizadas para cada campo do schema -> (:error/message)
+;; 2. Separar testes de: obrigatoriedade, tipo e valor (formato, tamanho, etc) 
+(deftest customer-schema-test
+  (testing "[OK] Entity with all the correct data"
+    (let [expected {:status :ok :data valid-customer :errors []}
+          output (schema-validate schema/CustomerSchema valid-customer)]
+      (is (= expected output))))
+  (testing "[FAIL] Entity with all invalid fields"
+    (let [output (schema-validate schema/CustomerSchema invalid-customer)]
+      (is (seq (:errors output))))))
