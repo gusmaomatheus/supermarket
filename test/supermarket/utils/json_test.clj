c468c835b02668c0c4d7450c8f72540941fe059d
Matheus Gusmão <mgusmao.dev@gmail.com>
2026-01-15 00:51:06 -0300

    test(json): add json to edn converting tests

--- test/supermarket/utils/json_test.clj
+++ test/supermarket/utils/json_test.clj
@@ -1,6 +1,16 @@
 (ns supermarket.utils.json-test
   (:require [clojure.test :refer [deftest is testing]]
-            [supermarket.utils.json :refer [edn->json json->edn]]))
+            [supermarket.utils.json :refer [edn->json json->edn]])
+  (:import [java.io ByteArrayInputStream InputStream]))
+
+;; helpers
+
+(defn- str->stream
+  "Creates an InputStream from a String to simulate the Ring's body."
+  [string]
+  (ByteArrayInputStream. (.getBytes string "UTF-8")))
+
+;; tests
 
 (deftest edn->json-test
   (testing "[OK] Simple map with multi data types."
@@ -10,3 +20,29 @@
   (testing "[OK] Map with nested keys."
     (is (= "{\"usuario\":{\"id\":1,\"roles\":[\"ADMIN\"]}}"
            (edn->json {:usuario {:id 1 :roles ["ADMIN"]}})))))
+
+(deftest json->edn-test
+  (testing "[OK] Must convert JSON keys (strings) to Keywords"
+    (let [json-input "{\"document\": \"123\", \"status\": \"OK\"}"]
+      (is (= {:document "123" :status "OK"}
+             (json->edn (str->stream json-input))))))
+
+  (testing "[OK] Must handle snake_case correctly by converting it to the kebab-case keyword."
+    (let [json-input "{\"registration_date\": \"2025-01-01\"}"]
+      (is (= {:registration-date "2025-01-01"}
+             (json->edn (str->stream json-input))))))
+
+  (testing "[OK] Complex nested structures"
+    (let [json-input "{\"data\": {\"items\": [{\"id\": 1}, {\"id\": 2}]}}"]
+      (is (= {:data {:items [{:id 1} {:id 2}]}}
+             (json->edn (str->stream json-input))))))
+
+  (testing "[OK] JSON arrays become Clojure vectors."
+    (let [json-input "[10, 20, 30]"]
+      (is (= [10 20 30]
+             (json->edn (str->stream json-input))))))
+
+  (testing "[OK] Unicode/accentuation handling"
+    (let [json-input "{\"mensagem\": \"Olá Mundo\"}"]
+      (is (= {:mensagem "Olá Mundo"}
+             (json->edn (str->stream json-input)))))))
