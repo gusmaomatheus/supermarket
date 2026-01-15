68556c00a20da70ca31a6b19f5170eb1453dd131
Matheus Gusmão <mgusmao.dev@gmail.com>
2026-01-15 00:21:15 -0300

    build: add json encoding/decoding lib

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.12.2"]
+                 [metosin/jsonista "0.3.13"]
                  [metosin/malli "0.20.0"]]
   :main ^:skip-aot supermarket.core
   :target-path "target/%s"
