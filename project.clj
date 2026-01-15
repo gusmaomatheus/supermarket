4896c4c27e40b6fdcc4dd5beb462b82d0dede448
Matheus Gusmão <mgusmao.dev@gmail.com>
2026-01-15 00:32:16 -0300

    build: add http server lib

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.12.2"]
                  [metosin/jsonista "0.3.13"]
-                 [metosin/malli "0.20.0"]]
+                 [metosin/malli "0.20.0"]
+                 [ring/ring-jetty-adapter "1.15.3"]]
   :main ^:skip-aot supermarket.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
