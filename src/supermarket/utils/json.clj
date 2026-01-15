f140b10bcf713b91a6e613a35fd2950126f5fc7e
Matheus Gusmão <mgusmao.dev@gmail.com>
2026-01-15 00:54:38 -0300

    fix(utils/json): correcting JSON to EDN conversion without using keywords and kebab-case

--- src/supermarket/utils/json.clj
+++ src/supermarket/utils/json.clj
@@ -1,15 +1,21 @@
 (ns supermarket.utils.json
-  (:require [jsonista.core :as json]))
+  (:require [clojure.string :refer [replace]]
+            [jsonista.core :as json]))
 
-(def ^:private object-mapper
-  (json/object-mapper {:decode-fn-key true}))
+(def ^:private custom-object-mapper
+  (json/object-mapper
+   {:decode-key-fn
+    (fn [k]
+      (-> k
+          (replace "_" "-")
+          keyword))}))
 
 (defn edn->json
   "Convert Clojure structures to JSON strings."
   [data]
-  (json/write-value-as-string data object-mapper))
+  (json/write-value-as-string data custom-object-mapper))
 
 (defn json->edn
   "Convert JSON strings to Clojure structures."
   [string]
-  (json/read-value string object-mapper))
+  (json/read-value string custom-object-mapper))
